d0851a3023b379c9bcd8a64a5f7e29bbfb5d154c
mcamp <usmcamp0811@gmail.com>
2019-01-27 15:42:53 +0000

    updated the setup.py to be adifferent version

--- setup.py
+++ setup.py
@@ -2,17 +2,17 @@ from setuptools import setup, find_packages
 
 setup(
     name='debacl',
-    version='1.1',
-    description='DEnsity-BAsed CLustering',
+    version='1.101',
+    description='DEnsity-BAsed CLustering Updated for Python 3',
     url='https://github.com/CoAxLab/DeBaCl',
     author='Brian Kent',
     author_email='bpkent@gmail.com',
     license='BSD',
     classifiers=[
         'License :: OSI Approved :: BSD License',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 2 :: Only',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3 :: Only',
         'Topic :: Scientific/Engineering',
         'Topic :: Scientific/Engineering :: Mathematics',
         'Topic :: Scientific/Engineering :: Visualization'
